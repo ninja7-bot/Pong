a346e3011cb1d88c03c8062575326643fba4026c
Dark Samurai <keshav223.keshav@gmail.com>
2025-01-09 02:10:03 +0530

    Shifting to Classes.

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 love = require "love"
 push = require 'push'
+Class = require 'class'
 
+require 'Paddle'
+require 'Ball'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -25,22 +28,12 @@ function love.load()
     
     push:setupScreen(VWIDTH, VHEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, { fullscreen = false, resizable = false, vsync = true })
 
-    -- Score track for the players
-    p1_score = 0
-    p2_score = 0
+    -- Instantiate Player Paddles.
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VWIDTH - 10, VHEIGHT - 30, 5, 20)
 
-    --These Y values keep the track of the movements of the Paddles.
-    p1_Y = 30
-    p2_Y = VHEIGHT - 50
-
-
-    -- ballX and ballY are the random values of X and Y to place the ball at.
-    ballX = VHEIGHT / 2 - 2
-    ballY = VWIDTH / 2 - 2
-
-    -- ballDX and ballDY are the velocities of the ball at X and Y coordinates.
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50, 50)
+    -- Instantiate ball.
+    ball = Ball(VWIDTH/2-2, VHEIGHT/2-2, 5, 5)
 
     gameState = 'start'
 end
@@ -49,21 +42,27 @@ function love.update(dt)
     -- This is the main game loop. It is called every frame.
     -- Here, we can update the game state, and also check for collisions.
     if love.keyboard.isDown('w') then --signifies the paddle moving up for player 1
-        p1_Y = math.max(0, p1_Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then --signifies the paddle moving down for player 1
-        p1_Y = math.min(VHEIGHT - 20, p1_Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
     
     if love.keyboard.isDown('up') then --signifies the paddle moving up for player 2
-        p2_Y = math.max(0, p2_Y + -PADDLE_SPEED * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then --signifies the paddle moving down for player 2
-        p2_Y = math.min(VHEIGHT - 20, p2_Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else 
+        player1.dy = 0
     end
 
     if gameState == 'play' then -- At the gameState play, the ball's movement starts.
-        ballX = ballX + ballDX * dt -- this decides the position at X coordinate.
-        ballY = ballY + ballDY * dt -- this decides the position at Y coordinate.
+        ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 function love.keypressed(key)
@@ -74,13 +73,8 @@ function love.keypressed(key)
             gameState = 'play'
         else
             gameState = 'start'  
-            -- Reinitialize the ball at the center.
-            ballX = VWIDTH / 2 - 2
-            ballY = VHEIGHT / 2 - 2
-
-            -- Initialize the ball's movements.
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50, 50) * 1.5
+            
+            ball:reset()
         end
     end
 end
@@ -89,6 +83,7 @@ function love.draw() --Rendering
 
     push:apply('start')
 
+    love.window.setTitle('PONG! By Dark Samurai')
     -- Setting a color to the background. Grey, here.
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
@@ -101,10 +96,18 @@ function love.draw() --Rendering
     end
 
     -- Creating the two paddles.
-    love.graphics.rectangle('fill', 10, p1_Y, 5, 20) -- paddle 1 now moves based on p1_y
-    love.graphics.rectangle('fill', VWIDTH - 10 , p2_Y, 5, 20) -- paddle 2 now moves based on p2_Y
+    player1:render()
+    player2:render()
     
-    love.graphics.rectangle('fill', ballX, ballY, 5, 5) -- ball
+    ball:render()
     
+    displayFPS()
+
     push:apply('end')
 end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: '.. tostring(love.timer.getFPS()), 10, 10)
+end    
